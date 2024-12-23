06cfaa47e868d1fd3c30685357897180d76a56f3
demo-23home <zeyadslama23@gmail.com>
2024-12-23 16:05:04 +0200

    Adding User(Creation, Editing) Admin Forms

--- core_apps/accounts/admin.py
+++ core_apps/accounts/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm, UserCreationForm
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = [
+        "pkid",
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_superuser",
+    ]
+
+    search_fields = ["first_name", "last_name", "email", "username"]
+
+    list_display_links = ["pkid", "id", "username", "email"]
+    ordering = ["pkid"]
+
+    # fieldsets is list of fields to be displayed in the user admin change form,
+    # each is a tuple contains a title and a dict witch contains the fields to be displayed.
+    fieldsets = (
+        (_("Login Credentials"), {"fields": ("email", "password")}),
+        (
+            _("Personal Information"),
+            {"fields": ("first_name", "last_name", "username")},
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_superuser",
+                    "is_staff",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    # add_fieldsets is list of fields to be displayed in the user admin create form.
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",), # Css styling for takeing the full width of the field.
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "username",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+    )
--- core_apps/accounts/models.py
+++ core_apps/accounts/models.py
@@ -1,3 +1,52 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
+from django.utils.translation import gettext_lazy as _
+from core_apps.accounts.managers import UserManager
 
-# Create your models here.
+
+class UsernameValidator(validators.RegexValidator):
+    regex = r"^[\w.@+-]+\Z"
+    message = _(
+        "Your username is not valid. A username should only contain letters , numbers, a dot "
+        "@ Symbol, + symbol and a dash"
+    )
+    flags = 0
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        USER = "USER", "User"
+
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    first_name = models.CharField(verbose_name=_("First Name"), max_length=50)
+    last_name = models.CharField(verbose_name=_("Last Name"), max_length=50)
+    email = models.EmailField(verbose_name=_("Email"), max_length=254, unique=True, db_index=True)
+    username = models.CharField(
+        verbose_name=_("Username"),
+        max_length=50,
+        unique=True,
+        validators=[UsernameValidator],
+    )
+
+    role = models.CharField(_("Role"), choices=Role.choices, blank=True, null=True)
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    manager = UserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+        ordering = ["-date_joined"]
+
+    @property
+    def get_full_name(self) -> str:
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip()
